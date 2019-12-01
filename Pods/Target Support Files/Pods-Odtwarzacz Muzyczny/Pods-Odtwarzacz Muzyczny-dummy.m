a0e6b6f3a4f6d18cbc9ad36ecb3b32a6be9e28d9
Adam Wienconek <adamwienconek@gmail.com>
2019-12-01 15:12:37 +0100

    Instalacja zewnętrznych bibliotek

---
+++ Pods/Target Support Files/Pods-Odtwarzacz Muzyczny/Pods-Odtwarzacz Muzyczny-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Odtwarzacz_Muzyczny : NSObject
+@end
+@implementation PodsDummy_Pods_Odtwarzacz_Muzyczny
+@end
---
+++ Pods/Target Support Files/Pods-Odtwarzacz MuzycznyTests/Pods-Odtwarzacz MuzycznyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Odtwarzacz_MuzycznyTests : NSObject
+@end
+@implementation PodsDummy_Pods_Odtwarzacz_MuzycznyTests
+@end
