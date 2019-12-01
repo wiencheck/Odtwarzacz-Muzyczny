04085f4acb4e74dfb1e2de4408073b84a13a4ae6
Adam Wienconek <adamwienconek@gmail.com>
2019-12-01 15:01:32 +0100

    Utworzenie projektu

---
+++ Odtwarzacz MuzycznyTests/Odtwarzacz_MuzycznyTests.swift
@@ -0,0 +1,34 @@
+//
+//  Odtwarzacz_MuzycznyTests.swift
+//  Odtwarzacz MuzycznyTests
+//
+//  Created by Adam Wienconek on 01/12/2019.
+//  Copyright © 2019 Adam Wienconek. All rights reserved.
+//
+
+import XCTest
+@testable import Odtwarzacz_Muzyczny
+
+class Odtwarzacz_MuzycznyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
